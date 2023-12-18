6d20843688a06479f1539d063bdba08aa2f755a8
Thomas <thomas.gorisse07@gmail.com>
2023-12-18 13:11:35 +0100

    init backend app

---
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/TodoListAppApplication.java
@@ -0,0 +1,13 @@
+package fr.gorisse.todoApp.TodoListApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/gorisse/todoApp/TodoListApp/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gorisse.todoApp.TodoListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
