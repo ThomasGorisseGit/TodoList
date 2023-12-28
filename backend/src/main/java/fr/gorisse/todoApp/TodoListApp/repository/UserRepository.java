15a8cc5fc4f424c0c2841155c943f619aa7f0c09
Cramara <cramara@users.noreply.github.com>
2023-12-28 11:23:41 +0100

    Ajout des getters + création d'un User

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/repository/UserRepository.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/repository/UserRepository.java
@@ -2,9 +2,13 @@ package fr.gorisse.todoApp.TodoListApp.repository;
 
 import fr.gorisse.todoApp.TodoListApp.entity.User;
 
+import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByEmail(V_Email email);
+    User findByUsername(String username);
 }
--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/UserService.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/UserService.java
@@ -1,7 +1,39 @@
 package fr.gorisse.todoApp.TodoListApp.services;
 
+import fr.gorisse.todoApp.TodoListApp.entity.User;
+import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import fr.gorisse.todoApp.TodoListApp.repository.UserRepository;
 
 @Service
 public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByEmail(V_Email email) {
+        return userRepository.findByEmail(email);
+    }
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    public boolean alreadyExistUserById(Integer id) {
+        return userRepository.existsById(id);
+    }
+    public User createUser(User user) {
+        if (user.getIdUser() != null && alreadyExistUserById(user.getIdUser())) {
+            throw new RuntimeException("User already exist");
+        }
+        return userRepository.save(user);
+    }
+
+
+
 }
