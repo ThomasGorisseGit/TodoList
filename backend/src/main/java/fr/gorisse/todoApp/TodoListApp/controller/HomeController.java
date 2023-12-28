d3a84fd7e747c94470e46e7bf59fc7e011c6ebab
gt102769 <thomas.gorisse@etu.unice.fr>
2023-12-28 09:51:06 +0100

    [Backend] EmailConverter

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package fr.gorisse.todoApp.TodoListApp.controller;
 
 import fr.gorisse.todoApp.TodoListApp.entity.User;
+import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,12 @@ import java.util.List;
 @RestController
 public class HomeController {
 
-    /*
+
     @GetMapping("/")
     public User home() {
-        return new User("Machin", "Truc", "machintruc");
+        return new User(1,"","","", V_Email.createEmail("thomas.gorisse08@gmail.com"),"",null,null,null);
     }
-
+    /*
     @GetMapping("/secured")
     public String secured() {
         return "Hello World Secured";
--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/entity/User.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/entity/User.java
@@ -1,9 +1,14 @@
 package fr.gorisse.todoApp.TodoListApp.entity;
 
 
+import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
+import fr.gorisse.todoApp.TodoListApp.entity.value_objects.converter.EmailConverter;
 import jakarta.persistence.*;
 import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.lang.NonNullFields;
+
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -24,9 +29,13 @@ public class User{
     private String firstName;
     private String username;
 
+
+    @Convert(converter = EmailConverter.class)
+    private V_Email email;
+
     // Todo : convert as value object
     private String password;
-    private String email;
+
     private String phone;
 
 
