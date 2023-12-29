2fdcdf72149cc30c783732df7229920f75290515
Cramara <cramara@users.noreply.github.com>
2023-12-29 13:34:10 +0100

    Implémentation de la class + ajout de getTodoListByIdUser

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
@@ -1,8 +1,10 @@
 package fr.gorisse.todoApp.TodoListApp.controller;
 
+import fr.gorisse.todoApp.TodoListApp.entity.TodoList;
 import fr.gorisse.todoApp.TodoListApp.entity.User;
 import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
 import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Phone;
+import fr.gorisse.todoApp.TodoListApp.services.TodoListService;
 import fr.gorisse.todoApp.TodoListApp.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +15,28 @@ import java.util.List;
 public class HomeController {
 
     private final UserService userService;
+    private final TodoListService todoListService;
 
-    public HomeController(UserService userService){
+    public HomeController(UserService userService, TodoListService todoListService){
         this.userService = userService;
+        this.todoListService = todoListService;
     }
 
 
     @GetMapping("/")
     public User home() {
-        return new User(1,"","","", V_Email.createEmail("thomas.gorisse08@gmail.com"),"", V_Phone.createPhone("0786910105"),null);
+        return new User(1,"","","", V_Email.createEmail("thomas.gorisse08@gmail.com"),"", V_Phone.createPhone("1234567890"),null);
     }
-    @GetMapping("/api/findUserFromList/{id}")
-    public List<User> getUserFromList(@PathVariable("id")int id){
+    @GetMapping("/api/followersList/{idList}")
+    public List<User> getUserFromList(@PathVariable("idList")int id){
         return this.userService.findUserFromList(id);
     }
-    @GetMapping("/api/findUserFromList")
+
+    @GetMapping("/api/homePageList/{idUser}")
+    public List<TodoList> getTodoListFromIdUser(@PathVariable("idUser")int id){
+        return this.todoListService.getTodoListByIdUser(id);
+    }
+    @GetMapping("/api/followersList")
     public List<User> getUserFromListwithrequestParam(@RequestParam int id){
         return this.userService.findUserFromList(id);
     }
--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/TodoListService.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/TodoListService.java
@@ -1,8 +1,21 @@
 package fr.gorisse.todoApp.TodoListApp.services;
 
+import fr.gorisse.todoApp.TodoListApp.entity.TodoList;
+import fr.gorisse.todoApp.TodoListApp.repository.TodoListRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 
 public class TodoListService {
+
+    TodoListRepository todoListRepository;
+
+    public TodoListService(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+    public List<TodoList> getTodoListByIdUser(Integer id) {
+        return todoListRepository.findTodoListsByIdUser(id);
+    }
 }
