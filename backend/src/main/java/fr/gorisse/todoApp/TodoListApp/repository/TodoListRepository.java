f3361562b833f4afeac30aba12aed7f6b657d76c
Cramara <cramara@users.noreply.github.com>
2023-12-29 13:37:49 +0100

    Création de la nouvelle requete qui permet d'avoir la liste des TodoList active d'un user d'un user

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/repository/TodoListRepository.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/repository/TodoListRepository.java
@@ -1,9 +1,19 @@
 package fr.gorisse.todoApp.TodoListApp.repository;
 
 import fr.gorisse.todoApp.TodoListApp.entity.TodoList;
+import fr.gorisse.todoApp.TodoListApp.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoListRepository extends CrudRepository<TodoList, Integer> {
+
+    @Query("SELECT tl " +
+            "FROM TodoList tl " +
+            "JOIN UseTable u ON tl.idTodoList = u.useId.todoList " +
+            "WHERE u.useId.followers = :idUser and u.isEnable = 'true' ")
+    List<TodoList> findTodoListsEnableByIdUser(Integer idUser);
 }
