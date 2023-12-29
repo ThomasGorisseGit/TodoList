58e9dc0c7ab4c445fc68a35d7b942115b498b988
gt102769 <thomas.gorisse@etu.unice.fr>
2023-12-29 10:53:29 +0100

    [Backend] Ajout de la route pour tester
    @Pathvariable : http://localhost:8080/api/findUserFromList/{id}
    
    @RequestParam : http://localhost:8080/api/findUserFromList?id={id}

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
@@ -3,10 +3,8 @@ package fr.gorisse.todoApp.TodoListApp.controller;
 import fr.gorisse.todoApp.TodoListApp.entity.User;
 import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Email;
 import fr.gorisse.todoApp.TodoListApp.entity.value_objects.V_Phone;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import fr.gorisse.todoApp.TodoListApp.services.UserService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +12,25 @@ import java.util.List;
 @RestController
 public class HomeController {
 
+    private final UserService userService;
+
+    public HomeController(UserService userService){
+        this.userService = userService;
+    }
+
 
     @GetMapping("/")
     public User home() {
         return new User(1,"","","", V_Email.createEmail("thomas.gorisse08@gmail.com"),"", V_Phone.createPhone("0786910105"),null);
     }
-
+    @GetMapping("/api/findUserFromList/{id}")
+    public List<User> getUserFromList(@PathVariable("id")int id){
+        return this.userService.findUserFromList(id);
+    }
+    @GetMapping("/api/findUserFromList")
+    public List<User> getUserFromListwithrequestParam(@RequestParam int id){
+        return this.userService.findUserFromList(id);
+    }
     @PostMapping("/addUser")
     public User addUser(
             @RequestBody User user
