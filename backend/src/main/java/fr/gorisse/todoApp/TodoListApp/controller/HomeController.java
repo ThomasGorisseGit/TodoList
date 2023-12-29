8a9f1377863b5c3403b071b8d38dbd898aa0e064
gt102769 <thomas.gorisse@etu.unice.fr>
2023-12-29 19:17:34 +0100

    [Backend] Set all user informations in userController

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/controller/HomeController.java
@@ -22,51 +22,14 @@ public class HomeController {
         this.todoListService = todoListService;
     }
 
-
     @GetMapping("/")
     public User home() {
         return new User(1,"","","", V_Email.createEmail("thomas.gorisse08@gmail.com"),"", V_Phone.createPhone("1234567890"),null);
     }
-    @GetMapping("/api/followersList/{idList}")
-    public List<User> getUserFromList(@PathVariable("idList")int id){
-        return this.userService.findUserFromList(id);
-    }
 
-    @GetMapping("/api/homePageList/{idUser}")
-    public List<TodoList> getTodoListFromIdUser(@PathVariable("idUser")int id){
-        return this.todoListService.getTodoListEnableByIdUser(id);
-    }
-    @GetMapping("/api/followersList")
-    public List<User> getUserFromListWithRequestParam(@RequestParam int id){
-        return this.userService.findUserFromList(id);
-    }
-    @PostMapping("/addUser")
-    public User addUser(
-            @RequestBody User user
-    ){
-        return userService.addUser(user);
-    }
 
-    @PostMapping("/addStringUser")
-    public User addStringUser(
-            @RequestParam String lastName,
-            @RequestParam String firstName,
-            @RequestParam String username,
-            @RequestParam V_Email email,
-            @RequestParam String password,
-            @RequestParam V_Phone phone
-    ){
-        return this.userService.addStringUser(lastName, firstName, username, email, password, phone);
-    }
 
-    @PostMapping("/UserPage/modifyUsername")
-    public User modifyUsername(
-            @RequestParam String username,
-            @RequestParam Integer idUser
-    ){
-        return this.userService.modifyUsername(username, idUser);
-    }
-
+    //todo : todolist RestController
     @PostMapping("/addTodoList")
     public TodoList addTodoList(
             @RequestBody TodoList todoList
@@ -74,7 +37,7 @@ public class HomeController {
 
         return this.todoListService.addTodoList(todoList);
     }
-
+    //Todo : on verra plus tard comment on fait / où on le met.
     @PostMapping("/api/addLinkBetweenUserAndTodoList")
     public boolean addLinkBetweenUserAndTodoList(
             @RequestBody LinkRequest linkRequest
@@ -84,13 +47,4 @@ public class HomeController {
         return this.todoListService.addLinkBetweenUserAndTodoList(user, todoList);
     }
 
-
-
-    /*
-    @GetMapping("/secured")
-    public String secured() {
-        return "Hello World Secured";
-    }
-
-   */
 }
