fdb0466edcdbf762c6b86dc6ec96baf1a1cbfee7
gt102769 <thomas.gorisse@etu.unice.fr>
2023-12-30 18:27:11 +0100

    [Backend] Exception status

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/exception/PasswordException.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/exception/PasswordException.java
@@ -1,5 +1,10 @@
-package fr.gorisse.todoApp.TodoListApp.controller.exception;
+package fr.gorisse.todoApp.TodoListApp.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class PasswordException extends RuntimeException{
     public PasswordException() {
         super("Le mot de passe doit contenir au moins 4 caractères dont une majuscule, une minuscule, un chiffre et un caractère spécial");
--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/exception/UsernameAlreadyExistException.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/exception/UsernameAlreadyExistException.java
@@ -1,6 +1,11 @@
-package fr.gorisse.todoApp.TodoListApp.controller.exception;
+package fr.gorisse.todoApp.TodoListApp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UsernameAlreadyExistException extends RuntimeException{
+
     public UsernameAlreadyExistException(String username) {
         super("Le nom d'utilisateur " + username + " est déjà utilisé");
     }
