1f0b01e4042b52545122c3e36ec559bc812ca732
gt102769 <thomas.gorisse@etu.unice.fr>
2024-01-02 10:51:26 +0100

    :lock: [Backend] Deletion

--- backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/TaskService.java
+++ backend/src/main/java/fr/gorisse/todoApp/TodoListApp/services/TaskService.java
@@ -1,20 +1,25 @@
 package fr.gorisse.todoApp.TodoListApp.services;
 
 import fr.gorisse.todoApp.TodoListApp.entity.Task;
+import fr.gorisse.todoApp.TodoListApp.entity.TodoList;
 import fr.gorisse.todoApp.TodoListApp.exception.TaskIntrouvableException;
 import fr.gorisse.todoApp.TodoListApp.repository.TaskRepository;
 import fr.gorisse.todoApp.TodoListApp.services.interfaces.IDeletion;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 
 public class TaskService implements IDeletion {
     TaskRepository taskRepository;
+    TodoListService todoListService;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository,TodoListService todoListService) {
         this.taskRepository = taskRepository;
+        this.todoListService = todoListService;
     }
 
     public Task findTaskById(Integer id) {
@@ -35,16 +40,25 @@ public class TaskService implements IDeletion {
 
 
 
-    // TODO Implement methods :
     @Override
     public void delete(int idCurrentEntity) {
-
+        Task toDelete = this.findTaskById(idCurrentEntity);
+        this.taskRepository.delete(toDelete);
     }
-    // TODO Implement methods :
+
+
     @Override
     public void deleteAll(int idParentEntity) {
 
+        // Fetch the todolist
+        TodoList list = this.todoListService.findTodoListById(idParentEntity);
+        List <Task> toDelete = new ArrayList<>(list.getTasks());
+        list.getTasks().clear();
+        this.taskRepository.deleteAll(toDelete);
+
     }
+
+
 
 
 
