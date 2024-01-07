76fb564bfe50170fbfd16f9715091dc1cc6905cb
gt102769 <thomas.gorisse@etu.unice.fr>
2024-01-07 18:36:47 +0100

    :fire: [frontend] Initialisation du composant LowerNavBar
    Co-Authored-By: cramara <118884264+cramara@users.noreply.github.com>

--- frontend/todolist-app/src/app/app.module.ts
+++ frontend/todolist-app/src/app/app.module.ts
@@ -1,18 +1,29 @@
+import {MatInputModule} from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LowerNavbarComponent } from './lower-navbar/lower-navbar.component';
-
+import { LowerNavbarComponent } from './navbar/lower-navbar/lower-navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    LowerNavbarComponent
+    LowerNavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+    RouterLink,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
