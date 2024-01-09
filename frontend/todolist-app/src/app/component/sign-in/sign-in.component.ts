a2920da05c982682641c4396af4b25d8afc4a57d
ThomasGorisseGit <thomas.gorisse07@gmail.com>
2024-01-09 22:40:47 +0100

    fin du style signIn

--- frontend/todolist-app/src/app/component/sign-in/sign-in.component.ts
+++ frontend/todolist-app/src/app/component/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+
+  formGroup : FormGroup = new FormGroup({
+    username : new FormControl(),
+    password : new FormControl(),
+  })
+
+  constructor(private authService:AuthService){
+    console.log(
+      );
+  }
+
+
+  connect(){
+    var userDetails : User = {
+      username:this.formGroup.controls["username"].value,
+      password:this.formGroup.controls["password"].value,
+      firstName:this.formGroup.controls["firstName"].value,
+      lastName:this.formGroup.controls["lastName"].value,
+      phone:this.formGroup.controls["phone"].value,
+      email:this.formGroup.controls["email"].value
+    };
+
+    this.authService.login(userDetails);
+  }
 
 }
